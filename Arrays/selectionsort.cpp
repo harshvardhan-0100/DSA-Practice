721f9bfb2755f0f13b2a9005af6e03b15ed6217b
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-06-11 09:34:54 +0530

    Initial DSA setup

---
+++ Arrays/selectionsort.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int mini = i; 
+        for (int j = 0; j < n; j++) {
+            if (arr[j] < arr[mini]) swap(arr[j], arr[mini]); 
+        }
+    }
+}
+int main() {
+    int n; 
+    cin >> n; 
+}
