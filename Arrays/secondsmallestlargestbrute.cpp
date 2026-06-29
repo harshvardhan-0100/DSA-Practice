f07c44452920e77523b8a9450d8a282f8afbeffb
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-06-29 16:58:30 +0530

    Added code and made program more safe by introducing new variables

--- Arrays/secondsmallestlargestbrute.cpp
+++ Arrays/secondsmallestlargestbrute.cpp
@@ -2,6 +2,11 @@
 using namespace std; 
 
 void secondls(int arr[], int n) {
+    // initializing variables
+    int secondMini = 0; 
+    int secondMaxi = 0; 
+
+    // sorting the array by selection sort
     for (int i = 0; i < n; i++) {
         int min = arr[i]; 
         for (int j = i + 1; j < n; j++) {
@@ -9,13 +14,28 @@ void secondls(int arr[], int n) {
         }
     }
 
-    cout << "Second largest element: " << arr[n - 2] << endl; 
-    cout << "Second smallest element: " << arr[1] << endl; 
+    for (int i = n - 2; i > 1; i--) {
+        if (arr[i] != arr[n - 1]) {
+            secondMaxi = arr[i]; 
+            break; 
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[0]) {
+            secondMini = arr[i]; 
+            break;
+        }
+    }
+
+    cout << "Second largest element: " << secondMaxi << endl; 
+    cout << "Second smallest element: " << secondMini << endl; 
 }
+
 int main() {
     int nums[12]; 
     for (int i = 0; i < 12; i++) {
         cin >> nums[i]; 
     }
-     secondls(nums, 12); 
+    secondls(nums, 12); 
 }
