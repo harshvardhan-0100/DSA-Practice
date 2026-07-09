b81010ed9eaaeb5dc33b8a27eb7df1775fd98a1f
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-07-09 10:33:32 +0530

    Remove fatal bug

--- Arrays/removeduplicatesbrute.cpp
+++ Arrays/removeduplicatesbrute.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
     int nums[8] = {1, 2, 2, 3, 4, 4, 5, 5};
+    int n = sizeof(nums) / sizeof(nums[0]);
 
     set<int> st; 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         st.insert(nums[i]); 
     }
 
@@ -15,10 +16,10 @@ int main() {
         index++; 
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < n; i++) {
         cout << nums[i] << " "; 
     } 
-    
+
 
     return index; 
 }
