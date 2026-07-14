69b8ca56e360d7a462feb176b2406242fc8ab774
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-07-14 22:09:55 +0530

    Preliminary attempt at array rotation to the right by k positions

---
+++ Arrays/rotaterightbykpos.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void getElements(vector<int> a) {
+    int n = a.size(); 
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " "; 
+    }
+    cout << endl;
+}
+
+void rightRotate(vector<int> nums, int k) {
+    int n = nums.size(); 
+    
+    k = k % n; 
+
+    cout << "Before rotation: "; 
+    getElements(nums); 
+
+    vector<int> temp; 
+
+    for (int i = n - k; i < n; i++) {
+        temp.push_back(nums[i]); 
+    }
+    cout << "The temporary array: "; 
+    getElements(temp); 
+
+    for (int i = 0; i < n - k; i++) {
+        nums[i + k] = nums[i];  
+    }
+
+    for (int i = 0; i < k; i++) {
+        nums[i] = temp[i]; 
+    }
+
+    cout << "After rotation: "; 
+    getElements(nums); 
+
+
+}
+
+int main() {
+    cout << "Program to right rotate an array by k places: "; 
+
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    rightRotate(nums, 3); 
+}
