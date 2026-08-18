40ffaddcdd3986826241c158c6c695afa12238db
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-18 23:37:42 +0530

    feat: skeletal program for the program to return the no. of maximum consecutive ones in an array

---
+++ Arrays/maxconsecones.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void maxOnes(vector<int> arr) {
+    int n = arr.size(); 
+
+    int count = 0; 
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            count += 1; 
+        }
+        if (arr[i] == 0) {
+            
+        }
+    }
+}
+
+int main() {
+
+}
