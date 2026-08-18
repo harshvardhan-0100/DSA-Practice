820a8dac64d5459046b6d69f89f703e6bc8e2233
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-18 08:10:10 +0530

    feat: corrected better approach program to find missing no.

--- Arrays/findmissingnobetter.cpp
+++ Arrays/findmissingnobetter.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int missing(vector<int> nums) {
     int n = nums.size(); 
 
-    vector<int> hash(n + 1, 0); 
+    vector<int> hash(n + 2, 0); 
 
     for (int i = 0; i < n; i++) {
         hash[nums[i]]++; 
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n + 1; i++) {
         if (hash[i] == 0) {
             cout << i; 
             return i; 
