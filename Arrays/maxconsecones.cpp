5ae23e68df4b82a9a823fbee8ba2e91afdda0e0f
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-19 08:07:07 +0530

    feat: correct approach code for finding maximum consecutive ones in a binary array; T.C.: O(n); S.C.: O(1)

--- Arrays/maxconsecones.cpp
+++ Arrays/maxconsecones.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
-void maxOnes(vector<int> arr) {
+int maxOnes(vector<int> arr) {
     int n = arr.size(); 
 
     int count = 0; 
+    int max_count = 0; 
 
     for (int i = 0; i < n; i++) {
         if (arr[i] == 1) {
             count += 1; 
         }
         if (arr[i] == 0) {
-            
+            count = 0; 
         }
+        max_count = max(count, max_count); 
     }
+
+    cout << max_count << endl; 
+    return max_count; 
 }
 
 int main() {
-
+    vector<int> bin = {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1}; 
+    maxOnes(bin); 
 }
