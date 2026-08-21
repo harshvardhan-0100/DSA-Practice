80bee90962c0130622a16e3e47b6b2af2042f720
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-21 12:10:35 +0530

    feat: add function to use unordered_map<int, int> to store frequency, works well for even negative values; renamed previous functions to accomodate diversity (lazy message Ik)

--- Arrays/singlenumber.cpp
+++ Arrays/singlenumber.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-int singleNumber(vector<int> nums) {
+int singleNumberHashing(vector<int> nums) {
     int max = INT_MIN; 
     for (int i = 0; i < nums.size(); i++) {
         if (nums[i] > max) {
@@ -23,8 +23,26 @@ int singleNumber(vector<int> nums) {
     return -1; 
 }
 
+int singleNumberMap(vector<int> nums) {
+    unordered_map<int, int> freq; 
+
+    for (int i = 0; i < nums.size(); i++) {
+        freq[nums[i]]++; 
+    }
+
+    for (auto it : freq) {
+        if (it.second == 1) {
+            return it.first; 
+        }
+    }
+
+    return -1; 
+}
+
 int main() {
     vector<int> nums = {4, 1, 2, 1, 2, 4, 57}; 
 
-    cout << "Single number from the array is: " << singleNumber(nums) << endl;
+    cout << "Single number from the array is: " << singleNumberHashing(nums) << endl;
+
+    cout << "Single number from the array is: " << singleNumberMap(nums) << endl;
 }
