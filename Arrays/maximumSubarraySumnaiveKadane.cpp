26baa0634f1783312185fd1de7c10d525df4f787
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-25 14:04:57 +0530

    docs: showcase naive Kadane's shortcomings in case of all negative arrays or if the max. subarray sum is negative

--- Arrays/maximumSubarraySumnaiveKadane.cpp
+++ Arrays/maximumSubarraySumnaiveKadane.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+int maxSubArray(vector<int>& nums) {
+    int maxi = INT_MIN; 
+    int sum = 0; 
 
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i]; 
+
+        if (sum < 0) sum = 0; // reinitialize sum to 0 once it gets negative
+
+        maxi = max(maxi, sum); 
+    }
+    return maxi; 
+}
 
 int main() {
+    vector<int> nums = {-5, -2, -8, -1}; 
 
+    cout << "The maximum subarray sum is: " << maxSubArray(nums) << endl;
 }
