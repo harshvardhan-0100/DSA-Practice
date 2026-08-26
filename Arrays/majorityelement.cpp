0b2ae82d93dc556bddae38ca9f32f681f33417a7
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-26 14:44:03 +0530

    feat: program to return the majority element from an array using hashmaps; T.C.: O(n logn), S.C.: O(n) (worst case)

--- Arrays/majorityelement.cpp
+++ Arrays/majorityelement.cpp
@@ -2,9 +2,23 @@
 using namespace std; 
 
 int majorityElement(vector<int> nums) {
-    
+    map<int, int> mpp; 
+
+    for (int i = 0; i < nums.size(); i++) {
+        mpp[nums[i]]++; 
+    }
+
+    for (auto it : mpp) {
+        if (it.second > (floor(nums.size()) / 2)); 
+        cout << it.first << endl;
+        return it.first; 
+    }
+    return -1; 
 }
 
 int main() {
+    vector<int> nums = {2, 3, 4, 5, 2, 3, 2, 3, 2, 3, 2, 2, 2, 2}; 
+    majorityElement(nums); 
 
+    return 0; 
 }
