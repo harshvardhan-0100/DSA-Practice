e688bdf0f90ba0021526651bf08d151596a4460c
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-26 09:11:33 +0530

    feat: brute force (low brain) program to sort colours

--- Arrays/sortcoloursandDNFalgo.cpp
+++ Arrays/sortcoloursandDNFalgo.cpp
@@ -1,10 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
+void getElements(vector<int> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+}
+
 void sortColours(vector<int> nums) {
-    
+    getElements(nums); 
+
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0; 
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 0) {
+            cnt0++; 
+        } else if (nums[i] == 1) {
+            cnt1++; 
+        } else {
+            cnt2++; 
+        }
+    }
+
+    for (int i = 0; i < cnt0; i++) {
+        nums[i] = 0; 
+    } 
+    for (int i = cnt0; i < cnt0 + cnt1; i++) {
+        nums[i] = 1; 
+    }
+    for (int i = cnt0 + cnt1; i < nums.size(); i++) {
+        nums[i] = 2; 
+    }
+
+    getElements(nums); 
 }
 
 int main() {
+    vector<int> nums = {0, 2, 1, 1, 2, 2, 2, 0, 1, 1, 0, 0}; 
 
+    sortColours(nums); 
 }
