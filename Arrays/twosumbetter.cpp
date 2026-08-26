4b6510db8f3ed844836147db2cb3190809799d0d
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-26 08:24:59 +0530

    feat: hashmap program to get the indices of the elements that add up to a 'target'

---
+++ Arrays/twosumbetter.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+vector<int> twoSum(vector<int> nums, int target) {
+    map<int, int>  mpp; 
+
+    for (int i = 0; i < nums.size(); i++) {
+        int num = nums[i]; 
+        int more = target - num; 
+
+        for (auto it : mpp) {
+            if (it.first == more) {
+                return {i, it.second}; 
+            }
+        }
+
+        mpp[nums[i]] = i; 
+    }
+    return {-1, -1}; 
+}
+
+int main() {
+    vector<int> nums = {2, 3, 4, 7, 9, 10}; 
+    int target = 17; 
+
+    vector<int> twoS = twoSum(nums, target); 
+    
+    for (int i = 0; i < twoS.size(); i++) {
+        cout << twoS[i] << " "; 
+    }
+    cout << endl; 
+}
