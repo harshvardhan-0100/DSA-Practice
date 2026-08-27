81f5408c341ba90891c17a6ed00d198e733b9942
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-27 08:03:14 +0530

    feat: brute force program to return the maximum subarray sum, by checking every possible subarray

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -2,9 +2,20 @@
 using namespace std; 
 
 int maximumSubarray(vector<int> nums) {
-    
+    int maxSum = INT_MIN;  
+    for (int i = 0; i < nums.size(); i++) {
+        int sum = 0;
+        for (int j = i; j < nums.size(); j++) { 
+            sum += nums[j]; 
+            maxSum = max(sum, maxSum); 
+        }
+    }
+
+    return maxSum; 
 }
 
 int main() {
+    vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3}; 
 
+    cout << maximumSubarray(nums) << endl; 
 }
