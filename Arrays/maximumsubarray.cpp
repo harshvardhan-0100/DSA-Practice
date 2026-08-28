aa5bb3009a1f5cbcb0851f587696b2663e06623a
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-28 07:18:35 +0530

    feat: The Kadane's Algorithm; focus on reinitializing sum once it is destined to give lower values in future because of negatives now

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-vector<int>  maximumSubarray(vector<int> nums) {
-    int maxSum = INT_MIN;  
-    int start = -1, end = -1; 
+int  maximumSubarray(vector<int> nums) {
+    int maxi = INT_MIN;  
+    int sum = 0; 
+
     for (int i = 0; i < nums.size(); i++) {
-        int sum = 0;
-        for (int j = i; j < nums.size(); j++) { 
-            sum += nums[j]; 
-            if (sum > maxSum) {
-                maxSum = sum;
-                start = i; 
-                end = j; 
-            } 
+        sum += nums[i]; 
+
+        if (sum < 0) {
+            sum = 0; // sum reinitialized to zero
         }
+
+        maxi = max(sum, maxi); 
     }
 
-    return {maxSum, start, end}; 
+    return maxi; 
+    
 }
 
 int main() {
     vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3}; 
 
-    vector<int> ans = maximumSubarray(nums); 
-
-    cout << "Max. sum: " << ans[0] << endl; 
-    cout << "Start index: " << ans[1] << endl; 
-    cout << "End index: " << ans[2] << endl; 
+    cout << "Max. subarray sum: " << maximumSubarray(nums) << endl;
 }
