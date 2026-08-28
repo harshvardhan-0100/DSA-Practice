983a4e33e2c1d49d99f43d9dba4ed9ed3eff6f3b
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-28 07:45:02 +0530

    feat: naive Kadane's algorithm implementation (doesn't work with all negative values; T.C.: O(n), S.C.: O(1)

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -4,23 +4,35 @@ using namespace std;
 int  maximumSubarray(vector<int> nums) {
     int maxi = INT_MIN;  
     int sum = 0; 
+    int start = -1, end = -1; 
+    int t_start = 0; 
 
     for (int i = 0; i < nums.size(); i++) {
+        if (sum == 0) t_start = i; 
+
         sum += nums[i]; 
 
         if (sum < 0) {
             sum = 0; // sum reinitialized to zero
         }
 
-        maxi = max(sum, maxi); 
+        if (sum > maxi) {
+            maxi = sum; 
+            start = t_start;
+            end = i; 
+        }
     }
+
+    cout << "The maximum subarray sum is: " << maxi << endl;
+    cout << "Starting index: " << start << endl;
+    cout << "Ending index: " << end << endl;
 
     return maxi; 
     
 }
 
 int main() {
-    vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3}; 
+    vector<int> nums = {-2, 4, -1, 5, 1, -6}; 
 
-    cout << "Max. subarray sum: " << maximumSubarray(nums) << endl;
+    maximumSubarray(nums); 
 }
