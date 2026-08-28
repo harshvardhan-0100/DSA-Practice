7cff4a8653422c323db763f418f5ee98699b8b51
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-28 08:08:28 +0530

    feat: added index handling and output along with optimised Kadane's algorithm

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -4,11 +4,33 @@ using namespace std;
 int  maximumSubarray(vector<int> nums) {
     int maxi = nums[0]; 
     int sum = nums[0]; 
+    int start = -1, end = -1;
+    int temp_start = -1; 
 
     for (int i = 1; i < nums.size(); i++) {
-        sum = max(nums[i], sum + nums[i]); 
-        maxi = max(sum, maxi);
+        // sum = max(nums[i], sum + nums[i]); 
+
+        if (nums[i] > sum + nums[i]) {
+            sum = nums[i]; 
+            temp_start = i; 
+        }
+
+        else {
+            sum = sum + nums[i]; 
+        }
+
+        // maxi = max(sum, maxi);
+
+        if (sum > maxi) {
+            maxi = sum; 
+            start = temp_start; 
+            end = i; 
+        }
     }
+
+    cout << "max. sum: " << maxi << endl;
+    cout << "start index: " << start << endl;
+    cout << "end index: " << end << endl;
 
     return maxi; 
 }
