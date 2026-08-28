a5a96ae1ae86dbdf5b072e9511cf08316268ca3d
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-08-28 07:09:01 +0530

    feat: brute force solution to maximum subarray problem; T.C.: O(n^2), S.C.: O(1)

--- Arrays/maximumsubarray.cpp
+++ Arrays/maximumsubarray.cpp
@@ -1,21 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-int maximumSubarray(vector<int> nums) {
+vector<int>  maximumSubarray(vector<int> nums) {
     int maxSum = INT_MIN;  
+    int start = -1, end = -1; 
     for (int i = 0; i < nums.size(); i++) {
         int sum = 0;
         for (int j = i; j < nums.size(); j++) { 
             sum += nums[j]; 
-            maxSum = max(sum, maxSum); 
+            if (sum > maxSum) {
+                maxSum = sum;
+                start = i; 
+                end = j; 
+            } 
         }
     }
 
-    return maxSum; 
+    return {maxSum, start, end}; 
 }
 
 int main() {
     vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3}; 
 
-    cout << maximumSubarray(nums) << endl; 
+    vector<int> ans = maximumSubarray(nums); 
+
+    cout << "Max. sum: " << ans[0] << endl; 
+    cout << "Start index: " << ans[1] << endl; 
+    cout << "End index: " << ans[2] << endl; 
 }
