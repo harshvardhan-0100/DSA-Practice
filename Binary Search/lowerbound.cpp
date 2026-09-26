428292f21ee5bf1aa15c094e3c70a4aa0c2391a1
Harsh Vardhan Rajput <vardhan.harsh08@protonmail.com>
2026-09-26 22:58:13 +0530

    feat: lower bound optimal program using binary search; T.C: O(log n)

--- Binary Search/lowerbound.cpp
+++ Binary Search/lowerbound.cpp
@@ -5,19 +5,27 @@ int lowerBound(vector<int> nums, int x) {
     // lower bound is the samllest index that has the value >= the target
     // sorted array and hence we can apply binary search
     // brute force is performing a linear search
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] >= x) {
-            return i; 
+    int low = 0, high = nums.size() - 1; 
+    int lb = nums.size(); 
+
+    while (low <= high) {
+        int mid = (low + high) / 2; 
+
+        if (x > nums[mid]) {
+            low = mid + 1; 
+        } else if (x <= nums[mid]) {
+            high = mid - 1; 
+            lb = mid; 
         }
     }
 
-    return -1; 
+    return lb; 
 }
 
 int main() {
     vector<int> nums = {3, 5, 8, 15, 19, 19, 19}; 
 
-    cout << "Lower bound for nums: " << lowerBound(nums, 14); 
+    cout << "Lower bound for nums: " << lowerBound(nums, 19); 
 
     return 0; 
 }
